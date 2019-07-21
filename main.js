b989ed1057d422bbd4ca9d3a84f506bc438c7f32
Max1Truc <maxitruc.bonbec@gmail.com>
2019-07-22 01:17:50 +0200

    commit lol

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function getCurrentFileExtension() {
+  var currentEditor = graviton.getCurrentEditor();
+  if (currentEditor != null) {
+    var filename = currentEditor.path.split("/").pop(),
+        extension = filename.split(".").pop();
+    return extension;
+  } else {
+    return "";
+  }
+}
+
+function run() {
+  var extension = getCurrentFileExtension();
+}
+
 const myPluginDropMenu = new dropMenu({
   id:"my_plugin_dm"
 });
